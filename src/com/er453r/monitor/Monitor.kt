99221302ae09afd9f09482714e04855041e45def
Marcin Kotz <marcin.kotz@ives-system.com>
2019-10-17 00:01:55 +0200

    fix main name

--- src/com/er453r/monitor/Monitor.kt
+++ src/com/er453r/monitor/Monitor.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class Monitorpplication
+class Monitor
 
 fun main(args: Array<String>) {
-    runApplication<Monitorpplication>(*args)
+    runApplication<Monitor>(*args)
 }
